6e52e89b523a344b42a21e95e640f7a221a32dce
Thiago Pavan <thiago.rpavan@mercadolivre.com>
2024-11-04 15:06:37 -0300

    feat(project): initial files (bun + hono)

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { Hono } from 'hono';
+import { logger } from 'hono/logger';
+
+const app = new Hono()
+app.use(logger());
+
+app.get('/', (c) => {
+  return c.json({ message: 'Hello World!' });
+})
+
+export default {
+  port: 8000,
+  fetch: app.fetch,
+}
